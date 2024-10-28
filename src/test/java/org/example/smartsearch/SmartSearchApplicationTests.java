20ea42931fc02e6544760d0df94bde4ffbaae4d0
czy <1791492943@qq.com>
2024-10-28 10:50:31 +0800

    1

---
+++ src/test/java/org/example/smartsearch/SmartSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
